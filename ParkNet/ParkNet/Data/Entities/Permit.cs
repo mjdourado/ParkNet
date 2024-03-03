af8ac8a62faa980944a1f9d0306f222458c6bfd2
Maria João Dourado <mjoaodourado@gmail.com>
2024-03-03 11:59:08 +0000

    GoingFine

--- ParkNet/ParkNet/Data/Entities/Permit.cs
+++ ParkNet/ParkNet/Data/Entities/Permit.cs
@@ -17,32 +17,6 @@ public class Permit
     public int ParkingSpaceId { get; set; }
     public ParkingSpace ParkingSpace { get; set; }
     public decimal Price { get; set; }
-
-
-    public Permit()
-    {
-        EndDate = GetEndDate();
-    }
-
-    public DateTime GetEndDate()
-    {
-        if (Type == PermitType.Monthly)
-        {
-            return StartDate.AddMonths(1);
-        }
-        else if (Type == PermitType.Quarterly)
-        {
-            return StartDate.AddMonths(3);
-        }
-        else if (Type == PermitType.Biannualy)
-        {
-            return StartDate.AddMonths(6);
-        }
-        else
-        {
-            return StartDate.AddYears(1);
-        }
-    }
 }
 
 public enum PermitType
@@ -51,15 +25,4 @@ public enum PermitType
     Quarterly,
     Biannualy,
     Yearly
-}
-
-public class PermitModel
-{
-    public int Id { get; set; }
-    public PermitType Type { get; set; }
-    public int VehicleId { get; set; }
-    public int ParkId { get; set; }
-    public int FloorId { get; set; }
-    public int ParkingSpaceId { get; set; }
-    public decimal Price { get; set; }
 }
